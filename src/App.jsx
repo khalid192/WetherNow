69ae2368fff6f821bac3badbdb06bdeadfd463ab
khalid192 <karazani2003@gmail.com>
2026-02-10 22:24:21 +0100

    chinge bachground

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { Box, Card, CardContent, } from "@mui/material";
-
+import './index.css'
 
 import Search from "./search";
 import Localdate from "./localdate";
@@ -14,11 +14,65 @@ import Card5days from "./card5days";
 import { useContext } from "react";
 import { IPContext } from "./context/contextIP.jsx";
 
-import sky from "./wethers_icone/sky.jpg"
+
+import P01d from "./background.jsx/P01d.jpg"
+import P01n from "./background.jsx/P01n.jpg"
+import P02d from "./background.jsx/P02d.jpg"
+import P02n from "./background.jsx/P02n.jpg"
+import P03n from "./background.jsx/P03n.jpg"
+import P03d from "./background.jsx/P03d.jpg"
+
+import P09n from "./background.jsx/P09n.jpg"
+import P09d from "./background.jsx/P09d.jpg"
+
+import P11d from "./background.jsx/P11d.jpg"
+import P11n from "./background.jsx/P11n.jpg"
+
+import P13d from "./background.jsx/P13d.jpg"
+import P13n from "./background.jsx/P13n.jpg"
+import P50n from "./background.jsx/P50n.jpg"
+import P50d from "./background.jsx/P50d.jpg"
+
+
+import undefinedP from "./background.jsx/undefined.jpg"
+
+
+
+
+
 
 function App() {
 
-  const { weather,  } = useContext(IPContext);
+  const { weather } = useContext(IPContext);
+  const iconCode = weather?.weather?.[0]?.icon;
+  console.log(iconCode)
+
+  const backgroundImages = {
+    "01d": P01d,
+    "01n": P01n,
+    "02d": P02d,
+    "02n": P02n,
+
+    "03d": P03d,
+    "03n": P03n,
+    "04d": P03d,
+    "04n": P03n,
+
+    "09d": P09d,
+    "09n": P09n,
+    "10d": P09d,
+    "10n": P09n,
+
+    "11d": P11d,
+    "11n": P11n,
+
+    "13d": P13d,
+    "13n": P13n,
+    "50d": P50d,
+    "50n": P50n,
+
+    "undefined":undefinedP
+  }
 
   const sx={
         display: "flex",
@@ -50,12 +104,13 @@ function App() {
     <>
        <Box
       sx={sx}
-       style={{backgroundImage: `url(${sky})`, backgroundSize: "cover", backgroundPosition: "center", }}  
+       style={{backgroundImage: `url(${ iconCode!=undefined?backgroundImages[iconCode]:backgroundImages.undefined})`, backgroundSize: "cover", backgroundPosition: "center",transition: "opacity 0.8s ease-in-out",
+      animation: "fadeIn 0.8s",}}  
     > 
     
      <Card5days/> 
       
-      <Card sx={{ minWidth: 275, padding: 1, textAlign: "right",backdropFilter: "blur(10px)", backgroundColor: "rgba(255, 255, 255, 0.3)", borderRadius: "15px" }}>
+      <Card sx={{ minWidth: 275, padding: 1, textAlign: "right",backdropFilter: "blur(10px)", backgroundColor: "rgba(255, 255, 255, 0.23)", borderRadius: "15px" }}>
 
         <CardContent sx={{padding:0}}>
 
