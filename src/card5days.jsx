0cf651413c11c79a20e513c5313700502cd933c5
khalid192 <karazani2003@gmail.com>
2026-02-13 13:45:38 +0100

    .

--- src/card5days.jsx
+++ src/card5days.jsx
@@ -94,7 +94,7 @@ export default function Card5days() {
               sx={{
                 backgroundColor: "rgba(183, 179, 179, 0.15)",
                 width: {xs:"40px",sm: "75px"},
-                p:0.3,
+                p:1,
                 height: {xs:"fitContent",sm: "95%"},
                 display: "flex",
                 flexDirection: "column",
