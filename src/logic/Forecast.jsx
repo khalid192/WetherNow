33e98dce597d968052fde8374e37d5226286bc8e
khalid192 <karazani2003@gmail.com>
2026-02-17 18:45:35 +0000

    .

--- src/logic/Forecast.jsx
+++ src/logic/Forecast.jsx
@@ -1,16 +1,16 @@
-
+const API_KEY = import.meta.env.VITE_WEATHER_KEY;
 
 
 
 export function getForecast(city, lang) {
   return fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&lang=${lang}&appid=3d4c2bf9a6c0fd81dde9d9f1c955e217`
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&lang=${lang}&appid=${API_KEY}`
   ).then(res => res.json());
 }
 
 
 export function getWeather(city, lang) {
   return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=${lang}&appid=3d4c2bf9a6c0fd81dde9d9f1c955e217`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=${lang}&appid=${API_KEY}`
   ).then(res => res.json());
 }
